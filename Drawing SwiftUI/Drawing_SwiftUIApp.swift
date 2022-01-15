32a33ec3d5fc83e2da81de83122840b0112fce37
VladimirRiabov <97258826+VladimirRiabov@users.noreply.github.com>
2022-01-15 22:54:53 +0300

    Initial Commit

---
+++ Drawing SwiftUI/Drawing_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Drawing_SwiftUIApp.swift
+//  Drawing SwiftUI
+//
+//  Created by Владимир Рябов on 15.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct Drawing_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
