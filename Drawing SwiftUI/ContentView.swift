ec4c837edd92cef0c02888a34903e3b6f72d3e33
VladimirRiabov <97258826+VladimirRiabov@users.noreply.github.com>
2022-01-16 13:12:47 +0300

    Transforming shapes using CGAffineTransform and even-odd fills – Drawing SwiftUI Tutorial 4/10

--- Drawing SwiftUI/ContentView.swift
+++ Drawing SwiftUI/ContentView.swift
@@ -7,47 +7,54 @@
 
 import SwiftUI
 
-struct Triangle: Shape {
+struct Flower: Shape {
+    //20 point away from the center
+    var petalOffSet = -20.0
+    //how wide is one petal
+    var petalWidth = 100.0
+    
     func path(in rect: CGRect) -> Path {
+        //all the petals
         var path = Path()
         
-        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
-        
+        //когда идет цикл он ставит точку каждый пи / 8
+        for number in stride(from: 0, to: Double.pi * 2, by: Double.pi / 16) {
+            //точка остается ось вращается или наоборот афинная трансформация
+            let rotation = CGAffineTransform(rotationAngle: number)
+            //объединение объединяет 2 афинный трансформации в нашем случае повернуть и сдвинуть
+            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
+            
+            let originalPetal = Path(ellipseIn: CGRect(x: petalOffSet, y: 0, width: petalWidth, height: rect.width / 2))
+            //применяем поворот и позицию
+            let rotatedPetal = originalPetal.applying(position)
+            
+            //добавляем это к нашей дорожке
+            path.addPath(rotatedPetal)
+        }
         return path
     }
 }
 
-struct Arc: InsettableShape {
-    let startAngle: Angle
-    let endAngle: Angle
-    //по часовой стрелке
-    let clockwise: Bool
-    var insetAmount = 0.0
-    
-    func path(in rect: CGRect) -> Path {
-        let rotationAdjustment = Angle.degrees(90)
-        let modifiedStartAngle = startAngle - rotationAdjustment
-        let modifiedEndAngle = endAngle - rotationAdjustment
-        var path = Path()
-        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2 - insetAmount, startAngle: modifiedStartAngle, endAngle: modifiedEndAngle, clockwise: !clockwise)
-        return path
-    }
-    
-    func inset(by amount: CGFloat) -> some InsettableShape {
-        var arc = self
-        arc.insetAmount += amount
-        return arc
-    }
-    
-}
 
 struct ContentView: View {
+    
+    @State private var petalOffset = -20.0
+    @State private var petalWidth = 100.0
+    
+    
     var body: some View {
-        Arc(startAngle: .degrees(-90), endAngle: .degrees(90), clockwise: true)
-            .strokeBorder(.blue, lineWidth: 40)
+        VStack {
+            Flower(petalOffSet: petalOffset, petalWidth: petalWidth)
+                .fill(.blue, style: FillStyle(eoFill: true, antialiased: false))
+//                .stroke(.red, lineWidth: 1)
+            Text("Offset")
+            Slider(value: $petalOffset, in: -40...40)
+              
+            Text("Width")
+            Slider(value: $petalWidth, in: -200...200)
+            
+        }
+        .padding([.horizontal, .bottom])
     }
 }
 
