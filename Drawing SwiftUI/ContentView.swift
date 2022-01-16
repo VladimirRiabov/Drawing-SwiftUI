6853c359762928f34fd59d5f381c3d9692e968fd
VladimirRiabov <97258826+VladimirRiabov@users.noreply.github.com>
2022-01-16 15:30:38 +0300

    Creative borders and fills using ImagePaint – Drawing SwiftUI Tutorial 5/10

--- Drawing SwiftUI/ContentView.swift
+++ Drawing SwiftUI/ContentView.swift
@@ -7,54 +7,25 @@
 
 import SwiftUI
 
-struct Flower: Shape {
-    //20 point away from the center
-    var petalOffSet = -20.0
-    //how wide is one petal
-    var petalWidth = 100.0
-    
-    func path(in rect: CGRect) -> Path {
-        //all the petals
-        var path = Path()
-        
-        //когда идет цикл он ставит точку каждый пи / 8
-        for number in stride(from: 0, to: Double.pi * 2, by: Double.pi / 16) {
-            //точка остается ось вращается или наоборот афинная трансформация
-            let rotation = CGAffineTransform(rotationAngle: number)
-            //объединение объединяет 2 афинный трансформации в нашем случае повернуть и сдвинуть
-            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
-            
-            let originalPetal = Path(ellipseIn: CGRect(x: petalOffSet, y: 0, width: petalWidth, height: rect.width / 2))
-            //применяем поворот и позицию
-            let rotatedPetal = originalPetal.applying(position)
-            
-            //добавляем это к нашей дорожке
-            path.addPath(rotatedPetal)
-        }
-        return path
-    }
-}
-
-
 struct ContentView: View {
     
-    @State private var petalOffset = -20.0
-    @State private var petalWidth = 100.0
-    
-    
+
     var body: some View {
         VStack {
-            Flower(petalOffSet: petalOffset, petalWidth: petalWidth)
-                .fill(.blue, style: FillStyle(eoFill: true, antialiased: false))
-//                .stroke(.red, lineWidth: 1)
-            Text("Offset")
-            Slider(value: $petalOffset, in: -40...40)
-              
-            Text("Width")
-            Slider(value: $petalWidth, in: -200...200)
-            
+            Text("Hello world")
+                 .frame(width: 300, height: 300)
+                 .border(ImagePaint(image: Image("Example"), sourceRect: CGRect(x: 0, y: 0.25, width: 1, height: 0.5), scale: 0.1), width: 50)
+     //            .border(.red, width: 30)
+                 .padding()
+            Capsule()
+                .strokeBorder(ImagePaint(image: Image("Example"), sourceRect: CGRect(x: 0, y: 0.25, width: 1, height: 0.5), scale: 0.1), lineWidth: 10)
+                
+                .frame(width: 300, height: 200)
+                .padding()
+                
         }
-        .padding([.horizontal, .bottom])
+        
+       
     }
 }
 
